6ec1c8ff19635c80570087dcc818d626210b2f56
Craig Russell <tenowg@gmail.com>
2013-06-13 20:11:19 -0400

    Update stratum_extended_layout.sql

--- sql/stratum_extended_layout.sql
+++ sql/stratum_extended_layout.sql
@@ -108,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `shares_archive` (
   `difficulty` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +132,26 @@ CREATE TABLE IF NOT EXISTS `shares_archive_found` (
   `difficulty` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE IF NOT EXISTS `shares_found` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTEMP ON UPDATE CURRENT_TIMESTAMP,
+  `rem_host` text,
+  `worker` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `our_result` tinyint(1) DEFAULT NULL,
+  `upstream_result` tinyint(1) DEFAULT NULL,
+  `solution` text,
+  `block_num` int(11) DEFAULT NULL,
+  `prev_block_hash` text,
+  `useragent` text,
+  `defficulty` int(11) DEFAULT NULL,
+  `username` text,
+  PRIMARY KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
